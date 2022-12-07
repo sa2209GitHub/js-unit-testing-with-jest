684bee052fa8f99c3c2a84dffbc1f0477dbdbc8d
Sergey Arkhangelskiy <sergey.arkhangelskiy@gmail.com>
2022-12-07 18:51:14 +0300

    Final commit

--- 03_async/async.spec.js
+++ 03_async/async.spec.js
@@ -1,4 +1,7 @@
+const { default: axios } = require('axios')
 const Ajax = require('./async')
+
+jest.mock('axios')
 
 describe('Ajax echo', () => {
     test('should return value using async/await syntax', async () => {
@@ -24,6 +27,32 @@ describe('Ajax echo', () => {
     test('should catch errors with promise', () => {
         return Ajax.echo().catch(err => {
             expect(err).toBeInstanceOf(Error)
+        })
+    })
+})
+
+describe('Ajax: GET', () => {
+    let response
+    let todos
+
+    beforeEach(() => {
+        todos = [{
+            id: 1,
+            title: 'Todo 1',
+            completed: false
+        }]
+        response = {
+            data: {
+                todos
+            }
+        }
+    })
+
+    test('should return data from backend', () => {
+        axios.get.mockReturnValue(response)
+
+        return Ajax.get().then(data => {
+            expect(data.todos).toEqual(todos)
         })
     })
 })
--- README.md
+++ README.md
@@ -2,13 +2,21 @@
 JavaScript Unit Testing With Jest
 
 1.  Init project with npm
-
     ```bash
     npm init [-y]
     ```
 
 2.  Install jest and @types/jest as development dependencies
-
     ```bash
     npm i -D jest @types/jest
     ```
+
+3.  Install axios
+    ```bash
+    mpm i axios
+    ```
+
+    1.  Intro:                      [code](./01_intro/intro.js)     [tests](./01_intro/intro.spec.js)
+    2.  Synchrous code examples:    [code](./02_sync/sync.js)       [tests](./02_sync/sync.spec.js)
+    3.  Acynchronous code examples: [code](./03_async/async.js)     [tests](./03_async/async.spec.js)
+    4.  Mock testing examples:      [code](./04_mock/mock.js)       [tests](./04_mock/mock.spec.js)
